3beb6e23eed71a94539e6a9722a9041f8c1d5da2
Sagar Pawar <sagarpawar7011098750@gmail.com>
2026-03-15 16:22:43 +0530

    added elements in js

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+const weatherDisplay = document.getElementById('weatherDisplay');
+const historyList = document.getElementById('historyList');
+const customConsole = document.getElementById('customConsole');
